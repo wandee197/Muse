6e301adf4bdc33888cbc56bd40da51fae7658637
MisterNT <cvbnm416@gmail.com>
2016-11-06 01:54:44 +0700

    muse

---
+++ muse.py
@@ -0,0 +1,19 @@
+
+s1=[1,2,3]
+s2=['q','p']
+s3=['a','b','c']
+
+
+def chrank(*vartuple):
+  maxr =0
+  for var in vartuple:
+     s5[int(len(var))-1][int(maxr)] = var[int(len(var))-1][int(maxr)]
+     maxr = int(maxr) + 1
+  return s5;
+   
+brs = chrank(s1,s2,s3)
+print (brs)
+
+
+
+     
